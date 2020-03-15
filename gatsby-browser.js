984c3859a0563c84d036ad4c9117a46cd0064749
burnpiro <kemalpiro@gmail.com>
2020-03-15 20:44:09 +0100

    Create gatsby base

---
+++ gatsby-browser.js
@@ -0,0 +1,10 @@
+/**
+ * Implement Gatsby's Browser APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/browser-apis/
+ */
+
+import "firebase/auth"
+import "firebase/database"
+import "firebase/functions"
+require('typeface-roboto');
