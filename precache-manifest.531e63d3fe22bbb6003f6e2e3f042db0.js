fa8285f3e7b9fbe5ca39c8854989ed72208fa337
burnpiro <kemalpiro@gmail.com>
2020-03-17 08:28:23 +0100

    Updates

---
+++ precache-manifest.531e63d3fe22bbb6003f6e2e3f042db0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89e492275bb48a91cc97e61452bd8906",
+    "url": "/people-matcher/index.html"
+  },
+  {
+    "revision": "7005c9eef34b28fd7fff",
+    "url": "/people-matcher/static/css/main.c732e76a.chunk.css"
+  },
+  {
+    "revision": "52dd22f0f94309b8609b",
+    "url": "/people-matcher/static/js/2.e10f980b.chunk.js"
+  },
+  {
+    "revision": "fc00e033e81f2099f316c15dad95575a",
+    "url": "/people-matcher/static/js/2.e10f980b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7005c9eef34b28fd7fff",
+    "url": "/people-matcher/static/js/main.6d4b0d2e.chunk.js"
+  },
+  {
+    "revision": "e5ffe9796f96432070aa",
+    "url": "/people-matcher/static/js/runtime-main.d428a1dc.js"
+  },
+  {
+    "revision": "2d7c9127960505744b6493252969ce68",
+    "url": "/people-matcher/static/media/google-icon.2d7c9127.png"
+  }
+]);
